4b9e0fdee5051c3f76ef0ad145e983ebf1f3341b
ashiruma <ashirumaabala@gmail.com>
2025-06-27 20:59:51 +0300

    Update urls.py

--- videos/urls.py
+++ videos/urls.py
@@ -1,11 +1,13 @@
-# videos/urls.py
-
 from django.urls import path
-from .views import RequestUploadURLView, FinalizeUploadView, RetryUploadURLView
+from .views import RequestUploadURLView, FinalizeUploadView, RetryUploadURLView, FilmListView
 
 app_name = 'videos'
 
 urlpatterns = [
+    # New public endpoint for listing films
+    path('', FilmListView.as_view(), name='film-list'),
+
+    # Existing creator-only endpoints
     path('request-upload/', RequestUploadURLView.as_view(), name='request-upload-url'),
     path('finalize-upload/', FinalizeUploadView.as_view(), name='finalize-upload'),
     path('retry-upload/', RetryUploadURLView.as_view(), name='retry-upload-url'),
